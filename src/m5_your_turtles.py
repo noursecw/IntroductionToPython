8e5e4e1016d357df3340e7e9cb754db6bbeb9335
noursecw <noursecw@rose-hulman.edu>
2017-11-27 16:47:49 -0500

    completed task

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Charles Nourse.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,26 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+steven = rg.SimpleTurtle('turtle')
+steven.pen = rg.Pen('green', .11)
+steven.speed = 30
+steven.left(90)
+
+geoff = rg.SimpleTurtle()
+geoff.pen = rg.Pen('orange', .7)
+geoff.speed = 12
+geoff.right(90)
+
+for k in range(173):
+    steven.draw_circle((k * 30) - k)
+    geoff.draw_circle((k * 30) + k)
+    steven.pen = rg.Pen('orange', .11 * k)
+    geoff.pen = rg.Pen('green', .7 * k)
+    steven.forward(k)
+    geoff.forward(k)
+
+window.close_on_mouse_click()
